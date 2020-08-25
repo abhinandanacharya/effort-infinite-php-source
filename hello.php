19a9cea0b30a588464dec302d25e5bb3c223dad7
abhinandan <acharyaabhinandan41@gmail.com>
2020-08-25 15:36:17 +0530

    first commit

---
+++ hello.php
@@ -0,0 +1,26 @@
+<?php
+//this is single comment
+/**vaiable with num values
+vaiable with num values
+vaiable with num values
+vaiable with num values
+vaiable with num values
+vaiable with num values
+vaiable with num values
+vaiable with num values
+vaiable with num values
+vaiable with num values
+$varname = "214789";**/
+
+
+
+?>
+<!--this html code-->
+<html>
+<head>
+	<title>Hello php</title>
+</head>
+<body>
+	<h1><?php  echo $varname; ?></h1>
+</body>
+</html>
